9b11b43af80372a088e7c164a4414ac66287595f
33b3ziz <egyptianhack325@gmail.com>
2023-12-05 01:27:20 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Nike Shoe Colletction Website
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+  Hero,
+  Subscribe,
+  Footer,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  SuperQuality,
+  CustomerReviews,
+};
